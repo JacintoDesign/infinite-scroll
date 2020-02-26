8abb0ab324d43e9d9a13d9103129e0700f99f26f
JACINTO WONG <jacinto.wong@cbc.ca>
2020-02-26 14:25:44 -0500

    getting data and logging it

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const grid = document.getElementById('grid');
 const loader = document.getElementById('loader');
 
+const count = 10;
+const accessKey = 'jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek';
+const apiEndpoint = `https://api.unsplash.com/photos/random?client_id=${accessKey}&count=${count}`
+
+async function getPhotos() {
+    const res = await fetch(apiEndpoint);
+    const data = await res.json();
+    console.log(data);
+    return data;
+}
+
+getPhotos();
