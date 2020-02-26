0d6fe32b96d201efafd19c17402eb38a4fe48fb8
JACINTO WONG <jacinto.wong@cbc.ca>
2020-02-26 14:58:07 -0500

    get photos into the DOM, update from flex to columns

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const count = 10;
 const accessKey = 'jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek';
 const apiEndpoint = `https://api.unsplash.com/photos/random?client_id=${accessKey}&count=${count}`
 
+// Get photos from Unsplash API
 async function getPhotos() {
     const res = await fetch(apiEndpoint);
     const data = await res.json();
@@ -12,4 +13,21 @@ async function getPhotos() {
     return data;
 }
 
-getPhotos();
+// Show photos in DOM
+async function updateDOM() {
+    const photos = await getPhotos();
+
+    photos.forEach(photo => {
+        grid.innerHTML += `
+        <div>
+            <a href="${photo.links.download}" target="_blank">
+                <img src="${photo.urls.regular}" alt="${photo.alt_description}" title="${photo.alt_description}" style="width: 100%">
+            </a>
+        </div>
+        `
+    }
+
+    )
+}
+
+updateDOM();
