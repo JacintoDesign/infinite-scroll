53ee0ff83aedd93cceb22bab89f84c4de127405f
JACINTO WONG <jacinto.wong@cbc.ca>
2020-02-27 08:52:25 -0500

    infinite scroll functionality

--- script.js
+++ script.js
@@ -1,33 +1,62 @@
 const grid = document.getElementById('grid');
 const loader = document.getElementById('loader');
 
+let ready = true;
+let photosArray = [];
 const count = 10;
 const accessKey = 'jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek';
 const apiEndpoint = `https://api.unsplash.com/photos/random?client_id=${accessKey}&count=${count}`
 
 // Get photos from Unsplash API
-async function getPhotos() {
-    const res = await fetch(apiEndpoint);
-    const data = await res.json();
-    console.log(data);
-    return data;
+function getPhotos() {
+    fetch(apiEndpoint)
+    .then(res => res.json())
+    .then(data => {
+        for (i = 0; i < data.length; i++) {
+            photoItem = data[i];
+            photoLink = data[i].urls.regular;
+            if (photoLink !== undefined) {               
+                photosArray.unshift(photoItem);
+            } else {
+                console.log('undefined photo');
+            }
+        }
+        console.log(photosArray);
+        displayPhotos();
+    })
 }
 
 // Show photos in DOM
-async function updateDOM() {
-    const photos = await getPhotos();
-
-    photos.forEach(photo => {
-        grid.innerHTML += `
+function displayPhotos() {
+    window.scroll(0, -5000);
+    grid.innerHTML = photosArray.map(
+        photo => `
         <div>
             <a href="${photo.links.download}" target="_blank">
                 <img src="${photo.urls.regular}" alt="${photo.alt_description}" title="${photo.alt_description}" style="width: 100%">
             </a>
         </div>
         `
-    }
-
-    )
+    ).join('');
+    setTimeout( () => {
+        loader.style.display = 'none';
+    }, 2000);
 }
 
-updateDOM();
+function loadMore() {
+    loader.style.display = 'block';
+    getPhotos();
+}
+
+window.addEventListener('scroll', function() {
+    if (((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 10) && ready) {
+        ready = false; 
+        setTimeout( () => {
+            ready = true;
+            console.log('ready');
+        }, 3000);
+        loadMore();
+    }
+  });
+
+getPhotos();
