7d7abe9c434ec31cc96047f6a644084601a6ee2c
JACINTO WONG <jacinto.wong@cbc.ca>
2020-03-03 12:55:16 -0500

    added native lazy load, modified functionality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const loader = document.getElementById('loader');
 
 let ready = true;
 let photosArray = [];
-const count = 10;
+const count = 30;
 const accessKey = 'jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek';
 const apiEndpoint = `https://api.unsplash.com/photos/random?client_id=${accessKey}&count=${count}`
 
@@ -16,7 +16,7 @@ function getPhotos() {
             photoItem = data[i];
             photoLink = data[i].urls.regular;
             if (photoLink !== undefined) {               
-                photosArray.unshift(photoItem);
+                photosArray.push(photoItem);
             } else {
                 console.log('undefined photo');
             }
@@ -28,12 +28,12 @@ function getPhotos() {
 
 // Show photos in DOM
 function displayPhotos() {
-    window.scroll(0, -5000);
+    // window.scroll(0, -5000);
     grid.innerHTML = photosArray.map(
         photo => `
         <div>
             <a href="${photo.links.download}" target="_blank">
-                <img src="${photo.urls.regular}" alt="${photo.alt_description}" title="${photo.alt_description}" style="width: 100%">
+                <img src="${photo.urls.regular}" alt="${photo.alt_description}" title="${photo.alt_description}" style="width: 100%" loading="lazy">
             </a>
         </div>
         `
@@ -49,7 +49,7 @@ function loadMore() {
 }
 
 window.addEventListener('scroll', function() {
-    if (((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 10) && ready) {
+    if (((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 500) && ready) {
         ready = false; 
         setTimeout( () => {
             ready = true;
