df4f02c5fc58753bbaa4715a3928658dcc66b7e9
JACINTO WONG <jacinto.wong@cbc.ca>
2020-04-25 17:07:48 -0400

    final production code

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
-const grid = document.getElementById("grid");
-const loader = document.getElementById("loader");
+const grid = document.getElementById('grid');
+const loader = document.getElementById('loader');
 
-let ready = true;
-let photosArray = [];
+const photosArray = [];
 const count = 30;
-const accessKey = "jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek";
-const apiEndpoint = `https://api.unsplash.com/photos/random?client_id=${accessKey}&count=${count}`;
+const apiKey = 'jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
-// Get photos from Unsplash API
-function getPhotos() {
-  fetch(apiEndpoint)
-    .then((res) => res.json())
-    .then((data) => {
-      for (i = 0; i < data.length; i++) {
-        photoItem = data[i];
-        photoLink = data[i].urls.regular;
-        if (photoLink !== undefined) {
-          photosArray.push(photoItem);
-        } else {
-          console.log("undefined photo");
-        }
-      }
-      console.log(photosArray);
-      displayPhotos();
-    });
+// Show/Hide Loader
+function toggleLoader() {
+  loader.hidden = !loader.hidden;
 }
 
-// Show photos in DOM
+// Send Photos to DOM Elements
 function displayPhotos() {
-  // window.scroll(0, -5000);
   grid.innerHTML = photosArray
     .map(
       (photo) => `
@@ -37,31 +21,46 @@ function displayPhotos() {
                 <img src="${photo.urls.regular}" alt="${photo.alt_description}" title="${photo.alt_description}" style="width: 100%" loading="lazy">
             </a>
         </div>
-        `
+        `,
     )
-    .join("");
-  setTimeout(() => {
-    loader.style.display = "none";
-  }, 2000);
+    .join('');
+  setTimeout(toggleLoader, 3000);
+}
+
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    for (let i = 0; i < data.length; i++) {
+      const photoItem = data[i];
+      photosArray.push(photoItem);
+    }
+    // console.log(photosArray);
+    displayPhotos();
+  } catch (error) {
+    // Catch Error Here
+  }
 }
 
 // Load More Photos, Show Loading Animation
 function loadMore() {
-  loader.style.display = "block";
+  toggleLoader();
   getPhotos();
 }
 
 // Check to see if scrolling near bottom of page, Load More
-window.addEventListener("scroll", function () {
+window.addEventListener('scroll', () => {
+  let ready = true;
   if (
-    window.innerHeight + window.scrollY >= document.body.offsetHeight - 500 &&
-    ready
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 500
+    && ready === true
   ) {
     ready = false;
     setTimeout(() => {
       ready = true;
-      console.log("ready");
-    }, 3000);
+      // console.log('ready');
+    }, 5000);
     loadMore();
   }
 });
