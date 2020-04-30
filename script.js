d8aced539dfc94611bf6cb7a03a26fc729326659
JACINTO WONG <jacinto.wong@cbc.ca>
2020-04-30 16:46:37 -0400

    final production code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const grid = document.getElementById('grid');
+const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
 let ready = false;
@@ -21,6 +21,13 @@ function imageLoaded() {
   }
 }
 
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (let key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 // Create Elements For Links & Images, Add to DOM
 function displayPhotos() {
   imagesLoaded = 0;
@@ -29,20 +36,24 @@ function displayPhotos() {
   for (let i = 0; i < imagesArray.length; i++) {
     // Create <a> to link to full photo
     const item = document.createElement('a');
-    item.setAttribute('href', imagesArray[i].links.download);
-    item.setAttribute('target', '_blank');
+    setAttributes(item, {
+      'href': imagesArray[i].links.download,
+      'target': '_blank'
+    });
     // Create <img> for photo
     const img = document.createElement('img');
-    img.setAttribute('src', imagesArray[i].urls.regular);
-    img.setAttribute('alt', imagesArray[i].alt_description);
-    img.setAttribute('title', imagesArray[i].alt_description);
-    img.setAttribute('width', '100%');
-    img.setAttribute('height', '100%');
+    setAttributes(img, {
+      'src': imagesArray[i].urls.regular, 
+      'alt': imagesArray[i].alt_description,
+      'title': imagesArray[i].alt_description,
+      'width': '100%',
+      'height': '100%',
+    });
     // Event Listener, check when each is finished loading
     img.addEventListener('load', imageLoaded);
-    // Put <img> inside <a>, then put both inside Grid Element
+    // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
-    grid.appendChild(item);
+    imageContainer.appendChild(item);
   }
 }
 
