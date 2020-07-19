e35e627d1644a8361ddd848a947f98347cee8fd6
JACINTO WONG <jacinto.wong@cbc.ca>
2020-07-19 14:06:28 -0400

    final production code

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let photosArray = [];
 
 // Unsplash API
 const count = 30;
+// Normally, don't store API Keys like this, but an exception made here because it is free, and the data is publicly available!
 const apiKey = 'jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
